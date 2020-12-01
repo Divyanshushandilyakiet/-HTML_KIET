f58333ce41051b7fdefee29cb9f7fcb3ac7d773e
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2020-12-02 04:33:43 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,10 @@
+// Synchronous and blocking
+// -line by line execution
+
+// Asynchronous and Non-
+// -line by line execution not sured 
+// - callbacks will fire 
+
+const fs = require("fs");
+let text = fs.readFile("dele.txt","utf-8");
+console.lo("This is a message");
